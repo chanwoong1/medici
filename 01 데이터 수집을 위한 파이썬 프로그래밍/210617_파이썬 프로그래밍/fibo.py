36849ff594610d1544b34704c7a154185798e10b
Chanwoong1 <84218652+Chanwoong1@users.noreply.github.com>
2021-11-07 21:13:56 +0900

    Add files via upload

---
+++ 01 데이터 수집을 위한 파이썬 프로그래밍/210617_파이썬 프로그래밍/fibo.py
@@ -0,0 +1,16 @@
+def fibo1(n) :
+    a, b = 0, 1
+    while b < n : 
+        print(b, end = ' ')
+        a, b = b, a+b
+    print()
+
+
+
+def fibo2(n) :
+    result = []
+    a, b = 0, 1
+    while b < n :
+        result.append(b)
+        a, b = b, a+b
+    return result
